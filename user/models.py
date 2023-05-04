8a3efd8fb8c13def76596ad57dc15012fdba4cbd
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-04 14:49:32 +0500

    add models

--- user/models.py
+++ user/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    pass
+
+
+class FriendshipRequest(models.Model):
+    """ Содержит информацию о запросах на дружбу между пользователями"""
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendship_requests_sent')
+    receiver = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendship_requests_received')
+    STATUS_CHOICES = [
+        ('P', 'Pending'),
+        ('A', 'Accepted'),
+        ('R', 'Rejected'),
+    ]
+    status = models.CharField(max_length=1, choices=STATUS_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class Friendship(models.Model):
+    """Содержит информацию о друзьях пользователей"""
+    user1 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendships1')
+    user2 = models.ForeignKey(User, on_delete=models.CASCADE, related_name='friendships2')
+    created_at = models.DateTimeField(auto_now_add=True)
