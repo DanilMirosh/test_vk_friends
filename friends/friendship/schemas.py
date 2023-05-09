bd1a29549bb19d621320a07e7ee8ac934d309e39
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-09 22:06:39 +0500

    add serv and schem friendship

---
+++ friends/friendship/schemas.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from typing import List
+from ninja import Schema
+
+from users.schemas import UserSchema
+
+
+class FriendshipStatus(str, Enum):
+    NONE: str = "none"
+    OUTGOING: str = "outgoing"
+    INCOMING: str = "incoming"
+    FRIENDS: str = "friends"
+
+
+class FriendshipStatusSchema(Schema):
+    """
+    Статусы дружбы: none/outgoing/incoming/friends
+    """
+    status: str
+
+
+class FriendshipRequestsSchema(Schema):
+    incoming: List[UserSchema]
+    outgoing: List[UserSchema]
