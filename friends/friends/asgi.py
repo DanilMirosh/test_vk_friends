11a1f29ec84816098f0bf4c3fe9801e299d15aff
DanilMirosh <miroshnichenko.da.vi@mail.ru>
2023-05-09 21:25:35 +0500

    start project friends

--- friends/friends/asgi.py
+++ friends/friends/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for vk_friends project.
+ASGI config for friends project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'vk_friends.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'friends.settings')
 
 application = get_asgi_application()
